9b33132685ecd0108dadff11bd2b389831e331af
Paulius Maruška <paulius.maruska@gmail.com>
2014-12-27 16:00:57 +0200

    Added setup scripts
    + Added: setup directory, containing install.sh, uninstall.sh and reinstall.sh
      Those are the main files used to install and uninstall my dotfiles.

---
+++ setup/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+cd "$(dirname "$0")/.."
+DOTFILES_ROOT=$(pwd)
+
+source $DOTFILES_ROOT/setup/functions.sh
+
+set -e
+
+install_dotfiles
---
+++ setup/reinstall.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+cd "$(dirname "$0")/.."
+DOTFILES_ROOT=$(pwd)
+
+source $DOTFILES_ROOT/setup/functions.sh
+
+set -e
+
+uninstall_dotfiles
+install_dotfiles

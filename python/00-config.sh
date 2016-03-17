9c3f1ab964a604038442f0bbe665f560fb9090f7
Paulius Maruška <paulius.maruska@gmail.com>
2016-03-17 06:58:34 +0200

    Added initialization of pyenv

--- python/00-config.sh
+++ python/00-config.sh
@@ -1,2 +1,8 @@
 # tell virtualenv not to mess with PS1
 VIRTUAL_ENV_DISABLE_PROMPT=yes
+
+# if $HOME/.pyenv exists, load pyenv stuff
+if [ -d "$HOME/.pyenv" ]; then
+    export PATH="/home/pam/.pyenv/bin:$PATH"
+    eval "$(pyenv init -)"
+fi

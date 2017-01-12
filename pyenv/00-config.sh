439f8841ce0cc006f26eafd7d69fc0cfd171b10e
Paulius Maruška <paulius.maruska@gmail.com>
2017-01-12 10:54:29 +0200

    Fixed pyenv path on MAC

--- pyenv/00-config.sh
+++ pyenv/00-config.sh
@@ -1,5 +1,5 @@
 # if $HOME/.pyenv exists, load pyenv stuff
 if [ -d "$HOME/.pyenv" ]; then
-    export PATH="/home/pam/.pyenv/bin:$PATH"
+    export PATH="$HOME/.pyenv/bin:$PATH"
     eval "$(pyenv init -)"
 fi

db12e1cdd9a0e39b92856f2af124e925e6066a15
Paulius Maruška <paulius.maruska@gmail.com>
2018-02-01 11:17:09 +0200

    Consolidated go and golang

--- go/00-config.sh
+++ go/00-config.sh
@@ -1,9 +1,13 @@
 # add go path if it exists
 # Note: on OS X go installs itself into /usr/local/go
+# it is also the default location for linux as well
 if [ -x /usr/local/go/bin/go ]; then
-    export PATH=/usr/local/go/bin:$PATH
+    export GOLANG_ROOT=/usr/local/golang
+    export GOROOT=/usr/local/go
+    export GOPATH=$GOLANG_PROJECTS
+    export PATH=$GOROOT/bin:$GOPATH/bin:$PATH
 fi
 
-if [ -d $PROJECTS/_golang ]; then
-    export GOPATH=$PROJECTS/_golang
+if [ -d $GOLANG_PROJECTS ]; then
+    export GOPATH=$GOLANG_PROJECTS
 fi

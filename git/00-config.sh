9e562d40c3e09145664b02f915af903ad3b9eac6
Paulius Maruška <paulius.maruska@gmail.com>
2018-06-12 13:47:55 +0300

    Added some git aliases

--- git/00-config.sh
+++ git/00-config.sh
@@ -7,3 +7,38 @@ if [ "$DOTFILESPLATFORM" = "Darwin" ]; then
         fpath+="$HOMEBREW_PREFIX/opt/git/share/zsh/site-functions"
     fi
 fi
+
+# setup git aliases
+# We do this, by removing current aliases, and then adding them again
+git config --global --remove-section alias 2>&1 >/dev/null
+
+# git debug <subcomand> ...
+# shows git trace on stderr for the given <subcommand> and its parameters
+git config --global alias.debug '!GIT_TRACE=2 git'
+
+# git alias <name> <value>
+# shortcut for defining a new alias
+git config --global alias.alias "!git config --global \"alias.\$1\" \"\$2\"  #"
+
+# shorter aliases for subcommands
+git alias br    branch
+git alias ci    commit
+git alias co    checkout
+git alias re    remote
+git alias st    status
+
+# shortening specific stuff
+git alias alist 'config --global --get-regexp "alias.*"'
+git alias brd   'branch -d'
+git alias brm   'branch -m'
+git alias cia   'commit --amend'
+git alias cim   'commit -m'
+git alias com   'checkout master'
+git alias last  'log -1 HEAD'
+git alias lg    'log --all --graph --decorate --oneline'
+git alias rea   'remote -v'
+git alias reu   'remote update'
+git alias reup  'remote update --prune'
+
+# outputs current branch
+git alias current 'rev-parse --abbrev-ref HEAD'

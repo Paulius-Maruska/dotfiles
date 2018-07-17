84fbb57478d8b2fd6948958673f35410169453e1
Paulius Maruška <paulius.maruska@gmail.com>
2018-07-17 10:00:16 +0300

    Moved git aliases into a separate file

---
+++ git/90-aliases.sh
@@ -0,0 +1,45 @@
+# We do this, by removing current aliases, and then adding them again
+git config --global --remove-section alias 2>&1 >/dev/null
+
+# git debug <subcomand> ...
+# shows git trace on stderr for the given <subcommand> and its parameters
+git config --global alias.debug '!GIT_TRACE=2 git'
+
+# git alias <name> <value>
+# shortcut for defining a new alias
+git config --global alias.alias "!git config --global \"alias.\$1\" \"\$2\"  #"
+
+# shorter aliases for subcommands
+git alias br       branch
+git alias ci       commit
+git alias co       checkout
+git alias re       remote
+git alias st       status
+
+# shortening specific stuff
+git alias alist    'config --global --get-regexp "alias.*"'
+git alias brd      'branch -d'
+git alias brm      'branch -m'
+git alias cia      'commit --amend'
+git alias cim      'commit -m'
+git alias cob      'checkout -b'
+git alias com      'checkout master'
+git alias last     'log -1 HEAD'
+git alias lg       'log --all --graph --decorate --oneline'
+git alias rea      'remote -v'
+git alias reu      'remote update'
+git alias reup     'remote update --prune'
+
+# outputs current branch
+git alias current  'rev-parse --abbrev-ref HEAD'
+
+# searches in branch names
+git alias branches "! a=\"\$1\" && shift && git rev-parse --abbrev-ref \"--branches=\$a\" \"\$@\"  #"
+
+# start new repository
+git alias start    "! a=\"\$1\" && shift && git init \"\$a\" && cd \"\$a\" && git commit --allow-empty --message 'Initial commit'"
+
+# output all commits that are included in specific merge(s)
+git alias prf      "! a=\"\$1\" && shift && for h in \$(git log --merges --pretty=format:%H --grep=\"\$a\"); do s=\"\$(git log -1 --merges --pretty=format:%P \"\$h\")\"; p=\"\$(git merge-base --octopus \$s)\"; git log \$p..\$h \"\$@\"; echo ''; done  #"
+git alias pr       "! a=\"\$1\" && shift && git prf \"\$a\" --pretty=oneline \"\$@\"  #"
+git alias prb      "! a=\"\$1\" && shift && git prf \"\$a\" --pretty=%H \"\$@\"  #"
